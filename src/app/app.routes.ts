2d5632a0f39c18f5c496daf4d8b7f905e8464dc3
BSOLI <bright.soli@computacenter.com>
2024-03-28 21:10:04 +0200

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AddressFormComponent } from './address-form/address-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'content', component: AddressFormComponent },
+  { path: 'analytics', component: AddressFormComponent },
+  { path: 'comments', component: AddressFormComponent },
+];
---
+++ src/app/navigation/navigation.component.ts
@@ -0,0 +1,72 @@
+import { Component, computed, inject, signal } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { AsyncPipe, CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+import { RouterModule, RouterOutlet } from '@angular/router';
+
+export type MenuItem = {
+  icon: string;
+  label: string;
+  route: string;
+}
+
+@Component({
+  selector: 'app-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrl: './navigation.component.scss',
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    AsyncPipe,
+    RouterOutlet,
+    RouterModule
+  ]
+})
+export class NavigationComponent {
+
+  private breakpointObserver = inject(BreakpointObserver);
+
+  menuItems = signal<MenuItem[]>([
+    {
+      icon: 'dashboard',
+      label: 'Dashboard',
+      route: 'dashboard',
+    },
+    {
+      icon: 'video_library',
+      label: 'Content',
+      route: 'content',
+    },
+    {
+      icon: 'analytics',
+      label: 'Analytics',
+      route: 'analytics',
+    },
+    {
+      icon: 'comment',
+      label: 'Comments',
+      route: 'comments',
+    }
+  ]);
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  collapsed = signal(false);
+
+  sidenavWidth = computed(() => this.collapsed() ? '65px' : "250px");
+}
